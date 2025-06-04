204a53388e3c25e90a1ef033788e57ef38da76f2
hitsan <soledewa2828@gmail.com>
2025-06-05 00:25:51 +0900

    launch server

--- test-server/server/main.ts
+++ test-server/server/main.ts
@@ -2,8 +2,10 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+let servers = [];
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Running test server')
 })
 
 Deno.serve(app.fetch)
