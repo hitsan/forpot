4caad3e79723b10142e8026a34c352aa8f5c4012
hitsan <soledewa2828@gmail.com>
2025-06-05 21:48:24 +0900

    add least function

--- test-server/server/main.ts
+++ test-server/server/main.ts
@@ -2,10 +2,46 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-let servers = [];
+let serverSession = {}
 
 app.get('/', (c) => {
-  return c.text('Running test server')
+  return c.json({ message:'Running test server', servers: serverSession}, 200)
 })
 
-Deno.serve(app.fetch)
+const getPort = async (c) => {
+  const data = await c.req.json()
+  return data.port
+}
+
+const launchServer = (port) => {
+  const app = new Hono()
+  const server = Deno.serve({ port: port }, app.fetch )
+  return server
+}
+
+const shutdownServer = (port) => {
+  const server = serverSession[port]
+  server.shutdown()
+}
+
+app.post(
+  '/servers/launch',
+  async (c) => {
+    const port = await getPort(c)
+    const server = launchServer(port)
+    serverSession[port] = server
+    return c.json({ port: port }, 200)
+  }
+)
+
+app.post(
+  '/servers/down',
+  async (c) => {
+    const port = await getPort(c)
+    shutdownServer(port)
+    delete serverSession[port]
+    return c.json({ port: port }, 200)
+  }
+)
+
+Deno.serve({port: 8000}, app.fetch)
