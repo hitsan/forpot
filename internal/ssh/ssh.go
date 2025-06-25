b0374d5ba456d71f7290f6134173a725f898af5f
hitsan <soledewa2828@gmail.com>
2025-06-26 02:02:54 +0900

    impl forwarding port

--- internal/ssh/ssh.go
+++ internal/ssh/ssh.go
@@ -14,10 +14,82 @@ import (
 )
 
 type ForwardSession struct {
-	uid    Uid
-	distIP net.IP
-	port   int
-	client *ssh.Client
+	listener   net.Listener
+	remoteAddr string
+	isClosed   bool
+}
+
+type SessionMG struct {
+	ip         string
+	client     *ssh.Client
+	sessionMap map[int]*ForwardSession
+}
+
+func NewForwardSession(localAddr string, remoteAddr string) (*ForwardSession, error) {
+	listener, err := net.Listen("tcp", localAddr)
+	if err != nil {
+		return nil, err
+	}
+	f := ForwardSession{
+		listener:   listener,
+		remoteAddr: remoteAddr,
+		isClosed:   false,
+	}
+	return &f, nil
+}
+
+func (f *ForwardSession) Close() error {
+	if !f.isClosed {
+		return errors.New("Already closed")
+	}
+	f.isClosed = true
+	return nil
+}
+
+func NewSessionMG(ip *net.IP, client *ssh.Client) *SessionMG {
+	return &SessionMG{
+		ip:         ip.String(),
+		sessionMap: make(map[int]*ForwardSession),
+		client:     client,
+	}
+}
+
+func (s *SessionMG) UpPorts(ports []int) []int {
+	var up []int
+	for _, port := range ports {
+		_, ok := s.sessionMap[port]
+		if ok {
+			continue
+		}
+		localAddr := fmt.Sprintf("127.0.0.1:%d", port)
+		remoteAddr := fmt.Sprintf("%s:%d", s.ip, port)
+		fs, err := NewForwardSession(localAddr, remoteAddr)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fs.forwardPort(s.client)
+		s.sessionMap[port] = fs
+	}
+	return up
+}
+
+func (s *SessionMG) DownPorts(ports []int) []int {
+	pm := make(map[int]struct{})
+	for _, port := range ports {
+		pm[port] = struct{}{}
+	}
+	var down []int
+	for port := range s.sessionMap {
+		_, ok := pm[port]
+		if ok {
+			continue
+		}
+		session := s.sessionMap[port]
+		session.Close()
+		delete(s.sessionMap, port)
+	}
+	return down
 }
 
 func CreateSshConfig(user string, password string) ssh.ClientConfig {
@@ -48,11 +120,20 @@ func fetchUid(client *ssh.Client) (Uid, error) {
 	return uid, nil
 }
 
-func InitSshSession(config ssh.ClientConfig, addr net.TCPAddr) error {
+func fetchProcNet(session *ssh.Session) (*string, error) {
+	var output bytes.Buffer
+	session.Stdout = &output
+	if err := session.Run("cat /proc/net/tcp"); err != nil {
+		return nil, err
+	}
+	p := output.String()
+	return &p, nil
+}
+
+func InitSshSession(config ssh.ClientConfig, addr net.TCPAddr, remoteIP net.IP) error {
 	client, err := ssh.Dial("tcp", addr.String(), &config)
 	if err != nil {
-		msg := "Failed to dial:" + addr.String()
-		return errors.New(msg)
+		return err
 	}
 	defer client.Close()
 
@@ -61,44 +142,37 @@ func InitSshSession(config ssh.ClientConfig, addr net.TCPAddr) error {
 		return err
 	}
 
+	sessionMG := NewSessionMG(&remoteIP, client)
 	for {
 		session, err := client.NewSession()
 		if err != nil {
-			log.Fatal("Failed to create new session", err)
+			log.Fatal(err)
+			continue
 		}
 		defer session.Close()
-
-		var output bytes.Buffer
-		session.Stdout = &output
-		if err := session.Run("cat /proc/net/tcp"); err != nil {
-			log.Println("Command errpr:", err)
+		pn, err := fetchProcNet(session)
+		if err != nil {
+			log.Fatal(err)
+			continue
 		}
-		ports := FindForwardablePorts(output.String(), uid)
-		for _, port := range ports {
-			forwardPort(client, addr.IP, port)
-		}
+		ports := FindForwardablePorts(*pn, uid)
+		go sessionMG.DownPorts(ports)
+		go sessionMG.UpPorts(ports)
 		time.Sleep(5 * time.Second)
 	}
 }
 
-func forwardPort(client *ssh.Client, distIP net.IP, port int) error {
-	localAddr := fmt.Sprintf("127.0.0.1:%d", port)
-	listener, err := net.Listen("tcp", localAddr)
-	if err != nil {
-		return err
-	}
-
-	dist := fmt.Sprintf("%s:%d", distIP.String(), port)
+func (f *ForwardSession) forwardPort(client *ssh.Client) error {
 	go func() {
 		for {
-			localConn, err := listener.Accept()
+			localConn, err := f.listener.Accept()
 			if err != nil {
-				fmt.Println("err")
+				fmt.Println(err)
 				continue
 			}
-			remoteConn, err := client.Dial("tcp", dist)
+			remoteConn, err := client.Dial("tcp", f.remoteAddr)
 			if err != nil {
-				fmt.Println("err")
+				fmt.Println(err)
 				continue
 			}
 			go io.Copy(remoteConn, localConn)
