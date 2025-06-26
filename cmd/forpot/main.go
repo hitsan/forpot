dde334ed84cef5203b2c6b93276e0aa383c2bec9
hitsan <soledewa2828@gmail.com>
2025-06-26 22:51:59 +0900

    impl parsing args

--- cmd/forpot/main.go
+++ cmd/forpot/main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"forpot/internal/ssh"
+	"log"
 	"net"
+	"os"
+	"os/user"
+	"strings"
+
+	"golang.org/x/term"
 )
+
+func parseHost(arg string) (string, string, error) {
+	items := strings.Split(arg, "@")
+	if len(items) > 2 {
+		return "", "", errors.New("Illigal connection")
+	}
+	if len(items) == 2 {
+		return items[0], items[1], nil
+	}
+	u, err := user.Current()
+	if err != nil {
+		return "", "", errors.New("Cannot get user name")
+	}
+	return u.Username, items[0], nil
+}
 
 func main() {
 	flag.Parse()
-	fmt.Println(flag.Args())
-	config := ssh.CreateSshConfig("root", "password")
+	args := flag.Args()
+	if len(args) != 1 {
+		log.Fatalln("Illigal args")
+		os.Exit(1)
+	}
+	user, host, err := parseHost(args[0])
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+	fmt.Print("Password:")
+	passwordsBytes, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+	fmt.Println()
+	password := string(passwordsBytes)
+	config := ssh.CreateSshConfig(user, password)
 	addr := net.TCPAddr{
-		IP:   net.ParseIP("127.0.0.1"),
+		IP:   net.ParseIP(host),
 		Port: 2222,
 	}
 	remoteAddr := net.ParseIP("127.0.0.1")
 	ssh.InitSshSession(config, addr, remoteAddr)
-	//fmt.Printf("config %+v", config)
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,12 @@
 module forpot
+
 //module github.com/hitsan/forpot
 
 go 1.24.1
 
 require (
-	golang.org/x/crypto v0.39.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/crypto v0.39.0
+	golang.org/x/term v0.32.0
 )
+
+require golang.org/x/sys v0.33.0 // indirect
